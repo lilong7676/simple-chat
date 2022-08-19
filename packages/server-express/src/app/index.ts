6bf68879795ea27cbed2aa7d9838f052b7daa554
Longlong Li <lilong7676@outlook.com>
2022-08-19 12:07:48 +0800

    refactor: 由于目前页面只适配了移动端的，所以非移动端访问的话，暂时返回一个由iframe包裹的页面

--- packages/server-express/src/app/index.ts
+++ packages/server-express/src/app/index.ts
@@ -3,12 +3,13 @@
  * @Author: lilonglong
  * @Date: 2022-04-27 22:59:52
  * @Last Modified by: lilonglong
- * @Last Modified time: 2022-08-19 10:43:36
+ * @Last Modified time: 2022-08-19 12:06:12
  */
 import express from 'express';
 import path from 'path';
 import http from 'http';
 import bodyParser from 'body-parser';
+import uaParser from 'ua-parser-js';
 import helmet from 'helmet';
 import cors from 'cors';
 import { AppDataSource } from '@app/data-source';
@@ -31,12 +32,30 @@ AppDataSource.initialize()
     const app = express();
 
     // TODO 优化点：使用配置文件，不要写死
-    app.set('views', path.resolve(__dirname, '../../../static/dist'));
+    app.set('views', [
+      path.resolve(__dirname, '../../../static/dist'),
+      path.resolve(__dirname, '../views'),
+    ]);
     app.set('view engine', 'ejs');
     app.get('/app', (req, res) => {
-      res.render('index', {
-        title: 'simple-chat',
-      });
+      const ua = uaParser(req.headers['user-agent']);
+      const deviceType = ua.device.type === 'mobile' ? 'mobile' : 'pc';
+
+      const reqUrl = `${req.protocol}://${req.get('host')}${req.originalUrl}`;
+      const reqUrlObj = new URL(reqUrl);
+
+      // 由于目前页面只适配了移动端的，所以非移动端访问的话，暂时返回一个由iframe包裹的页面
+      if (deviceType === 'mobile' || reqUrlObj.searchParams.has('mobile')) {
+        res.render('index', {
+          title: 'simple-chat',
+        });
+      } else {
+        reqUrlObj.searchParams.append('mobile', '1');
+        res.render('pc', {
+          title: 'simple-chat',
+          iframeSrc: reqUrlObj.toString(),
+        });
+      }
     });
 
     // 安全相关
